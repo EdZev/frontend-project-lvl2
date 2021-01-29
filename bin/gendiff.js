b424a80bafbb9fda0047d5b83e5ef63b8297de9e
EdZev <p89028084234@gmail.com>
2021-01-29 14:15:14 +0500

    #2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();

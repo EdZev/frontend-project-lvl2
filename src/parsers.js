118a3701e56efecafed3d306399340c33ca29f52
EdZev <p89028084234@gmail.com>
2021-02-06 14:37:29 +0500

    #5 added comparing YAML files

---
+++ src/parsers.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+export default (filePath) => {
+  try {
+    const format = path.extname(filePath).slice(1);
+    const data = fs.readFileSync(filePath, 'utf-8');
+    switch (format) {
+      case 'json':
+        return JSON.parse(data);
+      case 'yml':
+        return yaml.load(data);
+      default:
+        return `Unknown format of file: ${filePath}`;
+    }
+  } catch (err) {
+    return `No such file or directory: ${filePath}`;
+  }
+};
